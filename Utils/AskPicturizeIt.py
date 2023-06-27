253b9eda1d76058dcc716f282c2b0deccc4f7580
Amit Puri <e.amitpuri@gmail.com>
2023-06-27 20:48:30 +0530

    Update AskPicturizeIt.py

--- Utils/AskPicturizeIt.py
+++ Utils/AskPicturizeIt.py
@@ -205,20 +205,7 @@ class AskPicturizeIt:
 		                prompt_bond_girl.format(movie_name="Die Another Day")]
 
 
-    TEST_MESSAGE = """Rewrite this into a casual email:
-        
-Tis but thy name that is my enemy;
-Thou art thyself, though not a Montague.
-What’s Montague? It is nor hand, nor foot,
-Nor arm, nor face, nor any other part
-Belonging to a man. O, be some other name!
-What’s in a name? That which we call a rose
-By any other name would smell as sweet;
-So Romeo would, were he not Romeo call’d,
-Retain that dear perfection which he owes
-Without that title. Romeo, doff thy name,
-And for that name which is no part of thee
-Take all myself."""
+    TEST_MESSAGE = "My favorite TV shows are The Mentalist, The Blacklist, and Unforgettable. What are four series that I should watch next?"
 
     
     def get_wikimedia_image(self, keyword):
